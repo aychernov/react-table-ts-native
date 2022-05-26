f72cc7e8d2efbcba7ae3a4c3f2daa0e4f3b69a96
tallboy <yourspaceworkteam@gmail.com>
2022-05-26 17:39:07 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Table from "./Table";
+import axios from "axios";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fetchPostList = async () => {
+            const data = await axios('https://jsonplaceholder.typicode.com/users')
+            setPosts(data.data)
+        }
+        fetchPostList()
+    },[])
+
+    return (
+        <div>
+            <Table valueStart={0} valueEnd={2} posts={posts} whatkindofdata={['id', 'name', 'username', 'email']}/>
+            <Table valueStart={2} valueEnd={5} posts={posts} whatkindofdata={['id', 'name', 'username', 'email']}/>
+            <Table valueStart={5} valueEnd={7} posts={posts} whatkindofdata={['id', 'name', 'username', 'email']}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Table.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+interface posts{
+    id: any
+    name: any
+    username: any
+    email:any
+}
+
+interface data{
+    valueStart: number
+    valueEnd: number
+    posts: any
+    whatkindofdata: string[]
+}
+
+const Table = (props:data) => {
+    console.log(props)
+
+    return (
+        <table>
+             <thead>
+             <tr>
+                 <th>ID</th>
+                 <th>Name</th>
+                 <th>Username</th>
+                 <th>Email</th>
+             </tr>
+             </thead>
+            <tbody>
+            {
+                 props?.posts?.slice(props.valueStart, props.valueEnd).map((item:any) => (
+                    <tr key={item.id}>
+                        <td>{props.whatkindofdata.includes('id')? item.id : null}</td>
+                        <td>{props.whatkindofdata.includes('name')? item.name : null}</td>
+                        <td>{props.whatkindofdata.includes('username')? item.username : null}</td>
+                        <td>{props.whatkindofdata.includes('email')? item.email : null}</td>
+                    </tr>
+                ))
+            }
+            </tbody>
+        </table>
+
+    );
+};
+
+export default Table;
